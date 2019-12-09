86a54a70926be9088c9ed0dfcf05ffe1af799e5b
Marvin Reimer <therealmarv@gmail.com>
2019-12-09 18:19:49 +0200

    add xhtml mathml to LaTeX conversion for Autolatex plugin

---
+++ mathml2latex_script/baked_mathml2latex.py
@@ -0,0 +1,59 @@
+from lxml import etree
+import os
+import io
+
+def force_math_namespace_only(doc):
+    # http://wiki.tei-c.org/index.php/Remove-Namespaces.xsl
+    xslt='''<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns="http://www.w3.org/1998/Math/MathML">
+    <xsl:output method="xml" indent="no"/>
+
+    <xsl:template match="/|comment()|processing-instruction()">
+        <xsl:copy>
+          <xsl:apply-templates/>
+        </xsl:copy>
+    </xsl:template>
+
+    <xsl:template match="*">
+        <xsl:element name="{local-name()}">
+          <xsl:apply-templates select="@*|node()"/>
+        </xsl:element>
+    </xsl:template>
+
+    <xsl:template match="@*">
+        <xsl:attribute name="{local-name()}">
+          <xsl:value-of select="."/>
+        </xsl:attribute>
+    </xsl:template>
+    </xsl:stylesheet>
+    '''
+
+    xslt_doc = etree.parse(io.BytesIO(xslt))
+    transform = etree.XSLT(xslt_doc)
+    doc = transform(doc)
+    return doc
+
+def mathml2latex_yarosh(equation):
+    """ MathML to LaTeX conversion with XSLT from Vasil Yaroshevich """
+    script_base_path = os.path.dirname(os.path.realpath(__file__))
+    xslt_file = os.path.join(script_base_path, 'xsl_yarosh', 'mmltex.xsl')
+    dom = etree.fromstring(equation)
+    xslt = etree.parse(xslt_file)
+    transform = etree.XSLT(xslt)
+    newdom = transform(dom)
+    return unicode(newdom)
+
+def main():
+    f = etree.parse("2-5-Quadratic-Equations.xhtml")
+    ns = {"h": "http://www.w3.org/1999/xhtml",
+          "m": "http://www.w3.org/1998/Math/MathML"}
+    for r in f.xpath('//h:math', namespaces=ns):
+        math_etree = force_math_namespace_only(r)
+        equation = etree.tostring(math_etree, with_tail=False, inclusive_ns_prefixes=None)
+        # print(equation)
+        autolatex = '$' + mathml2latex_yarosh(equation) +'$'
+        r.tail = autolatex + r.tail if r.tail else autolatex
+    etree.strip_elements(f,'{http://www.w3.org/1999/xhtml}math',with_tail=False)
+    print(etree.tostring(f,pretty_print=True))
+
+if __name__== "__main__":
+    main()
